9f0a21a8de058a1cc337f0dcd94d0396182f90bd
unknown <adnanraad2002@gmail.com>
2026-09-24 20:32:16 +0600

    added

---
+++ Basic/list,tuple,dict.py
@@ -0,0 +1,13 @@
+a=["Apple",23,2.5,'Orange'] #index wise
+print(a[0],a[1],a[2],a[3])
+
+b=("mahib", "araf")
+print(b[1])
+
+raad={                  #key wise
+    "Name":"Adnan",
+    "Age": 25,
+    "Status": 'Student'
+}
+
+print(raad["Age"], raad['Status'])
